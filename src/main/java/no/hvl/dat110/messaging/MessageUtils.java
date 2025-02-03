f6310d0dc392247debdf69c457229934e56004c6
676627 <142162653+676627@users.noreply.github.com>
2025-02-03 21:35:23 +0100

    Complete task 1 - messaging

--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -1,9 +1,5 @@
 package no.hvl.dat110.messaging;
 
-import java.util.Arrays;
-
-import no.hvl.dat110.TODO;
-
 public class MessageUtils {
 
 	public static final int SEGMENTSIZE = 128;
@@ -12,37 +8,39 @@ public class MessageUtils {
 	public static String MESSAGINGHOST = "localhost";
 
 	public static byte[] encapsulate(Message message) {
-		
+
 		byte[] segment = null;
 		byte[] data;
-		
-		// TODO - START
-		
+
 		// encapulate/encode the payload data of the message and form a segment
 		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
-		// TODO - END
+		segment = new byte[SEGMENTSIZE];
+		data = message.getData();
+		segment[0] = (byte) data.length;
+
+		for (int i = 1; i < data.length + 1; i++) {
+			segment[i] = data[i - 1];
+		}
+
 		return segment;
-		
+
 	}
 
 	public static Message decapsulate(byte[] segment) {
 
 		Message message = null;
-		
-		// TODO - START
+
 		// decapsulate segment and put received payload data into a message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
+		byte[] payload = new byte[segment[0]];
+		int length = segment[0];
+		for (int i = 1; i < length + 1; i++) {
+			payload[i - 1] = segment[i];
+		}
+
+		message = new Message(payload);
+
 		return message;
-		
+
 	}
-	
+
 }
--- src/main/java/no/hvl/dat110/messaging/MessagingServer.java
+++ src/main/java/no/hvl/dat110/messaging/MessagingServer.java
@@ -2,8 +2,7 @@ package no.hvl.dat110.messaging;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-
-import no.hvl.dat110.TODO;
+import java.net.Socket;
 
 public class MessagingServer {
 
@@ -28,14 +27,16 @@ public class MessagingServer {
 
 		MessageConnection connection = null;
 
-		// TODO - START
-		// accept TCP connection on welcome socket and create messaging connection to be returned
+		// accept TCP connection on welcome socket and create messaging connection to be
+		// returned
 
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
+		try {
+			Socket connectionSocket = welcomeSocket.accept();
+			connection = new MessageConnection(connectionSocket);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return connection;
 
 	}
